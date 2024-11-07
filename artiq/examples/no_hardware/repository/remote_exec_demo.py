bce702d13940945f2e4830f626bd1d6c8c2c8e01
SilviaJoJo <zhou.fang@duke.edu>
2024-11-07 08:59:58 -0500

    Finished testing

--- artiq/examples/no_hardware/repository/remote_exec_demo.py
+++ artiq/examples/no_hardware/repository/remote_exec_demo.py
@@ -1,44 +1,56 @@
+"""
+Tutorial on how to run this code:
+
+1. Open a terminal and run the following command to start the remote execution controller:
+   python3 -m artiq.examples.remote_exec_controller
+
+2. In another terminal, run this script using the ARTIQ environment.
+
+This script demonstrates remote execution with ARTIQ, allowing you to run experiments either locally or remotely.
+"""
+
 import time
 import inspect
 
 from sipyco.remote_exec import connect_global_rpc
-
 from artiq.experiment import *
-
 import remote_exec_processing
-
 
 class RemoteExecDemo(EnvExperiment):
     def build(self):
-        self.setattr_device("camera_sim")
-        self.setattr_device("scheduler")
-        self.setattr_argument("remote_exec", BooleanValue(False))
-        self.setattr_argument("show_picture", BooleanValue(True), "Local options")
-        self.setattr_argument("enable_fit", BooleanValue(True), "Local options")
+        # Set up devices and arguments
+        self.setattr_device("camera_sim")  # Simulated camera device
+        self.setattr_device("scheduler")   # Scheduler device
+        self.setattr_argument("remote_exec", BooleanValue(False))  # Argument to enable remote execution
+        self.setattr_argument("show_picture", BooleanValue(True), "Local options")  # Option to show picture
+        self.setattr_argument("enable_fit", BooleanValue(True), "Local options")  # Option to enable fitting
         if self.remote_exec:
-            self.setattr_device("camera_sim_rexec")
+            self.setattr_device("camera_sim_rexec")  # Remote execution camera device
 
     def prepare(self):
+        # Prepare for remote execution if enabled
         if self.remote_exec:
-            connect_global_rpc(self.camera_sim_rexec)
+            connect_global_rpc(self.camera_sim_rexec)  # Connect to remote execution RPC
             self.camera_sim_rexec.add_code(
-                inspect.getsource(remote_exec_processing))
+                inspect.getsource(remote_exec_processing))  # Add processing code to remote execution
 
     def transfer_parameters(self, parameters):
+        # Transfer parameters to datasets
         w, h, cx, cy = parameters
-        self.set_dataset("rexec_demo.gaussian_w", w, archive=False, broadcast=True)
-        self.set_dataset("rexec_demo.gaussian_h", h, archive=False, broadcast=True)
-        self.set_dataset("rexec_demo.gaussian_cx", cx, archive=False, broadcast=True)
-        self.set_dataset("rexec_demo.gaussian_cy", cy, archive=False, broadcast=True)
+        self.set_dataset("rexec_demo.gaussian_w", w, archive=False, broadcast=True)  # Set Gaussian width
+        self.set_dataset("rexec_demo.gaussian_h", h, archive=False, broadcast=True)  # Set Gaussian height
+        self.set_dataset("rexec_demo.gaussian_cx", cx, archive=False, broadcast=True)  # Set Gaussian center x
+        self.set_dataset("rexec_demo.gaussian_cy", cy, archive=False, broadcast=True)  # Set Gaussian center y
 
     def fps_meter(self):
+        # Measure frames per second
         t = time.monotonic()
         if hasattr(self, "last_pt_update"):
             self.iter_count += 1
             dt = t - self.last_pt_update
             if dt >= 5:
-                pt = dt/self.iter_count
-                self.set_dataset("rexec_demo.picture_pt", pt, archive=False, broadcast=True)
+                pt = dt/self.iter_count  # Calculate period time
+                self.set_dataset("rexec_demo.picture_pt", pt, archive=False, broadcast=True)  # Set period time dataset
                 self.last_pt_update = t
                 self.iter_count = 0
         else:
@@ -46,29 +58,32 @@ class RemoteExecDemo(EnvExperiment):
             self.iter_count = 0
 
     def run_local(self):
+        # Run the experiment locally
         while True:
-            self.fps_meter()
-            data = self.camera_sim.get_picture()
+            self.fps_meter()  # Update FPS meter
+            data = self.camera_sim.get_picture()  # Get picture from simulated camera
             if self.show_picture:
                 self.set_dataset("rexec_demo.picture", data,
-                                 archive=False, broadcast=True)
+                                 archive=False, broadcast=True)  # Broadcast picture data
             if self.enable_fit:
-                p = remote_exec_processing.fit(data, self.get_dataset)
-                self.transfer_parameters(p)
-            self.scheduler.pause()
+                p = remote_exec_processing.fit(data, self.get_dataset)  # Fit Gaussian to data
+                self.transfer_parameters(p)  # Transfer fit parameters
+            self.scheduler.pause()  # Pause the scheduler
 
     def run_remote(self):
+        # Run the experiment remotely
         while True:
-            self.fps_meter()
-            p = self.camera_sim_rexec.call("get_and_fit")
-            self.transfer_parameters(p)
-            self.scheduler.pause()
+            self.fps_meter()  # Update FPS meter
+            p = self.camera_sim_rexec.call("get_and_fit")  # Call remote method to get and fit data
+            self.transfer_parameters(p)  # Transfer fit parameters
+            self.scheduler.pause()  # Pause the scheduler
 
     def run(self):
+        # Main run method to choose between local and remote execution
         try:
             if self.remote_exec:
-                self.run_remote()
+                self.run_remote()  # Run remotely if remote_exec is enabled
             else:
-                self.run_local()
+                self.run_local()  # Otherwise, run locally
         except TerminationRequested:
-            pass
+            pass  # Handle termination request gracefully
--- artiq/examples/no_hardware/repository/remote_exec_processing.py
+++ artiq/examples/no_hardware/repository/remote_exec_processing.py
@@ -3,44 +3,49 @@ from numba import jit
 from scipy.optimize import least_squares
 import logging
 
-
+# Set up logger for this module
 logger = logging.getLogger(__name__)
 
-
 @jit(nopython=True)
-def compute_gaussian(r, img_w, img_h,
-                     gaussian_w, gaussian_h,
-                     gaussian_cx, gaussian_cy):
+def compute_gaussian(r, img_w, img_h, gaussian_w, gaussian_h, gaussian_cx, gaussian_cy):
+    # Compute a 2D Gaussian distribution over the image dimensions
     for y in range(img_h):
         for x in range(img_w):
+            # Calculate the squared distance from the Gaussian center
             ds = ((gaussian_cx-x)/gaussian_w)**2
             ds += ((gaussian_cy-y)/gaussian_h)**2
+            # Compute the Gaussian value at (x, y)
             r[x, y] = np.exp(-ds/2)
 
-
 def fit(data, get_dataset):
-    img_w, img_h = data.shape
+    # Fit a Gaussian to the given data
+    img_w, img_h = data.shape  # Get image dimensions
 
     def err(parameters):
-        r = np.empty((img_w, img_h))
-        compute_gaussian(r, img_w, img_h, *parameters)
-        r -= data
-        return r.ravel()
-    guess = [
-        get_dataset("rexec_demo.gaussian_w", 12),
-        get_dataset("rexec_demo.gaussian_h", 15),
-        get_dataset("rexec_demo.gaussian_cx", img_w/2),
-        get_dataset("rexec_demo.gaussian_cy", img_h/2)
-    ]
-    res = least_squares(err, guess)
-    return res.x
+        # Error function for least squares optimization
+        r = np.empty((img_w, img_h))  # Create an empty array for the Gaussian
+        compute_gaussian(r, img_w, img_h, *parameters)  # Compute Gaussian with current parameters
+        r -= data  # Subtract the data from the Gaussian
+        return r.ravel()  # Return the flattened error array
 
+    # Initial guess for the Gaussian parameters
+    guess = [
+        get_dataset("rexec_demo.gaussian_w", 12),  # Width
+        get_dataset("rexec_demo.gaussian_h", 15),  # Height
+        get_dataset("rexec_demo.gaussian_cx", img_w/2),  # Center x
+        get_dataset("rexec_demo.gaussian_cy", img_h/2)   # Center y
+    ]
+    # Perform least squares optimization to fit the Gaussian
+    res = least_squares(err, guess)
+    return res.x  # Return the optimized parameters
 
 def get_and_fit():
+    # Get data and fit a Gaussian to it
     if "dataset_db" in globals():
         logger.info("using dataset DB for Gaussian fit guess")
 
         def get_dataset(name, default):
+            # Retrieve dataset value or use default if not found
             try:
                 return dataset_db.get(name)
             except KeyError:
@@ -49,5 +54,8 @@ def get_and_fit():
         logger.info("using defaults for Gaussian fit guess")
 
         def get_dataset(name, default):
+            # Always use default values
             return default
+
+    # Get picture data and fit Gaussian
     return fit(controller_driver.get_picture(), get_dataset)
